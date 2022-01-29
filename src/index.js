a150a090d24237e7545af026b49b76d32e6fb620
Julia-itcourse <juliaartemenko93@gmail.com>
2022-01-29 17:34:56 +0100

    init: solve the task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+  const result = [];
+
+  if (matrix === undefined) {
+    return [];
+  } else {
+    for (
+      let subArrayIndex = 0;
+      subArrayIndex < matrix.length;
+      subArrayIndex++
+    ) {
+      let isEven = subArrayIndex % 2 === 0;
+      for (
+        let indexInsideSubarray = 0;
+        indexInsideSubarray < matrix[subArrayIndex].length;
+        indexInsideSubarray++
+      ) {
+        let index;
+        isEven
+          ? (index = indexInsideSubarray)
+          : (index = matrix[subArrayIndex].length - indexInsideSubarray - 1);
+        result.push(matrix[subArrayIndex][index]);
+      }
+    }
+    return result;
+  }
+};
